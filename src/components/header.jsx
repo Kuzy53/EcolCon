9ca06f2490c5e66934ffaf47b6c9253819038b51
topi768 <topi768@inbox.ru>
2024-09-16 16:28:04 +1000

    .

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,17 +1,30 @@
+import styles from './Header.module.css';
 
 const Header = () => {
   return (
-    <header>
-      <h1>My App</h1>
-      <nav>
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact</a></li>
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <img src='assets/logo.png' className={styles.logo}></img>
+        <ul className={styles.navList}>
+          <li className={styles.navItem}>
+            <a href="#" className={styles.navLink}>новости</a>
+          </li>
+          <li className={styles.navItem}>
+            <a href="#" className={styles.navLink}>мероприятия</a>
+          </li>
+          <li className={styles.navItem}>
+            <a href="#" className={styles.navLink}>организация</a>
+          </li>
+          <li className={styles.navItem}>
+            <a href="#" className={styles.navLink}>войти</a>
+          </li>
+          <li className={`${styles.navItem} ${styles.regBtn}`}>
+            <a href="#" className={styles.navLink}>зарегистрироваться</a>
+          </li>
         </ul>
       </nav>
     </header>
   );
-};
+};е 
 
-export default Header;
+export default Header
